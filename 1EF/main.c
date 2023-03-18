c47a012ce0a57c9abe95a5a3b920f76e9866777d
Daniele Primon <ledebass@gmail.com>
2023-03-18 20:10:23 +0100

    Update main.c
    Initialize max in TrovareMassimo() so that the function could possibly return a negative value.

--- 1EF/main.c
+++ 1EF/main.c
@@ -30,6 +30,7 @@ void CaricareVettore(int x, int Vet[x]){
 
 int TrovareMassimo(int x, int Vet[x]){
     int max=0, i=0;
+    max=Vet[i];
     while(i<x){
         if (Vet[i]>max)
             max=Vet[i];
